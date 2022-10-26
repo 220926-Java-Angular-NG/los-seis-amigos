adba4cfef8584f56a751386643843fb2825d78b0
Chase Lopez <chase.opez16@gmail.com>
2022-10-25 23:10:22 -0500

    Removed 2 files, CardRepo now will create and load
    Two files removed
    	-NCReader
    		- parsed thru textfile from url path ussed in constructor and
    		  created a array of String arrys to store lines from
    		  url
    		- when line was collected from the url i was making
    		  cards using the CardRepo classes create method
    		- took code and move into Reteiver class as the double
    		  arry is not needed any more
    	-Reteriver
    		- took the double array from before and created text
    		  files in resource directory to be refrenced later
    			- this was my soultion before I was put onto databases
    		- since there was no longer a need to store info in the
    		  text files more than half the file was not needed
    	-CardRepo
    		- placed code from Reteriver after refoctoring the above
    		  classes and now can load data into the database using
    		  the loadTable method
    		- exist method returns true if the table exist in the
    		  database
    		- createTable runs sql command to create the card table
    		- loadTable has the code from Reteriver class but still
    		  has a few things to be refactored

--- Project2/src/main/java/org/project2/repo/CardRepo.java
+++ Project2/src/main/java/org/project2/repo/CardRepo.java
@@ -3,7 +3,11 @@ package org.project2.repo;
 import org.project2.model.Card;
 import org.project2.util.ConnectionManager;
 
+import java.io.*;
+import java.net.URL;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class CardRepo {
     // come back and implement crud dom interface
@@ -22,26 +26,88 @@ public class CardRepo {
         }
     }
 
-    private void loadTable() {
-    }
-
-    private void createTable() {
-    }
-
     private boolean exist() {
         try {
             String sql = "SELECT EXISTS (SELECT FROM cards)";
             PreparedStatement pstmt = con.prepareStatement(sql);
             ResultSet rs = pstmt.executeQuery();
-            if(rs.next()){
+            if (rs.next()) {
                 return true;
-            }else{
+            } else {
                 return false;
             }
             //return rs.next()?true:false; not sure if this would be preferred by if else above
         } catch (SQLException e) {
             System.out.println(e.getMessage());
             return false;
+        }
+    }
+
+    private boolean createTable() {
+        try {
+            String sql = "create table cards(cardid serial primary key," +
+                    "name varchar(141) not null," +
+                    "setname varchar(13)," +
+                    "imagefile varchar(51) not null unique," +
+                    "actualset varchar(4)," +
+                    "color varchar(5)," +
+                    "colorid varchar(5)," +
+                    "concost varchar(46)," +
+                    "manavalue varchar(7)," +
+                    "type varchar(49)," +
+                    "power varchar(10)," +
+                    "toughness varchar(3)" +
+                    ",loyalty varchar(1)," +
+                    "rarity varchar(1)," +
+                    "text varchar(907))";
+            PreparedStatement pstmt = con.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                if (this.exist()) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+            return false;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+    private void loadTable() {
+        BufferedReader inputFile;
+        String pathToResources = "src/main/resources/";
+        File cardInfoFolder = new File(pathToResources+"cardinfo/");
+        if (!cardInfoFolder.exists()) {
+            cardInfoFolder.mkdirs();
+        }
+        // Link below contains url's each containing a portion of all card information based of release data
+        // http://magicplugin.normalitycomics.com/update/cardFiles/
+        ArrayList<String> sitelist = new ArrayList<String>(); // Hold the 13 paths from url above
+        try {
+            // Hardcoded paths in text file located in resources folder
+            Scanner sc = new Scanner(new File(pathToResources+"sitelist.txt"));
+            // Iterate through
+            while (sc.hasNext()) {
+                String line = sc.nextLine();// Stores line from file in a string literal
+                //System.out.println("From site list file: " + line);// print pathToResources from sitelist
+                sitelist.add(line);// Adds pathToResources into list of url
+                try {
+                    inputFile = new BufferedReader(new InputStreamReader(new URL(line).openStream()));
+                    String lineFromFile;
+                    while ((lineFromFile = inputFile.readLine()) != null) {
+                        //System.out.println(lineFromFile);
+                        this.create(new Card(lineFromFile));
+                    }
+                } catch (IOException e) {
+                    System.out.println(e);
+                }
+                //nc.printMainDir();
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 
