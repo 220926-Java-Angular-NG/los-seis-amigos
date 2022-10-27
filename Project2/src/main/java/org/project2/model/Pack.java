26914437b32ef08bc175f298cc25893f49a218b6
Jonathan Bedoy <jonathanbedoy1@gmail.com>
2022-10-26 21:05:56 -0700

    completed cartRepo

--- Project2/src/main/java/org/project2/model/Pack.java
+++ Project2/src/main/java/org/project2/model/Pack.java
@@ -4,4 +4,35 @@ public class Pack {
     private int packId;
     private String setName;
 
+    public Pack() {
+    }
+
+    @Override
+    public String toString() {
+        return "Pack{" +
+                "packId=" + packId +
+                ", setName='" + setName + '\'' +
+                '}';
+    }
+
+    public Pack(int packId, String setName) {
+        this.packId = packId;
+        this.setName = setName;
+    }
+
+    public int getPackId() {
+        return packId;
+    }
+
+    public void setPackId(int packId) {
+        this.packId = packId;
+    }
+
+    public String getSetName() {
+        return setName;
+    }
+
+    public void setSetName(String setName) {
+        this.setName = setName;
+    }
 }
--- Project2/src/main/java/org/project2/repo/CartRepo.java
+++ Project2/src/main/java/org/project2/repo/CartRepo.java
@@ -1,34 +1,123 @@
 package org.project2.repo;
 
-import org.project2.model.Pack;
+import org.project2.model.CartItem;
+import org.project2.util.ConnectionManager;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 // TODO: implement skeleton code
 public class CartRepo {
+    Connection connection;
+
+    // trying out methods on this repo
+    public static void main(String[] args) {
+        CartRepo crepo = new CartRepo();
+        System.out.println(crepo.updateCartItemQuantity(1, 1, 1573));
+        System.out.println(crepo.getCartItemsByUserId(1));
+        System.out.println(crepo.removeFromCart(1, 1));
+        System.out.println(crepo.getCartItemsByUserId(1));
+        System.out.println(crepo.addItemToCart(1, 1, 321));
+        System.out.println(crepo.addItemToCart(1, 1, 321));
+        System.out.println(crepo.addItemToCart(1, 1, 321));
+        System.out.println(crepo.getCartItemsByUserId(1));
+//        System.out.println(crepo.emptyUserCartByUserId(1));
+        System.out.println(crepo.getCartItemsByUserId(1));
+    }
+
+    public CartRepo() {
+        try {
+            this.connection  = ConnectionManager.getConnection();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
     // will get the users cart from the DB using users -> userId
-    List<Pack> getCartItemsByUserId (int userId) {
+    List<CartItem> getCartItemsByUserId (int userId) {
+        try {
+        String sql = "SELECT * FROM cart JOIN packs on cart.packid = packs.packid where userId = ?";
+        PreparedStatement psmt = connection.prepareStatement(sql);
+        psmt.setInt(1, userId);
+        ResultSet rs = psmt.executeQuery();
+        List<CartItem> cartItems = new ArrayList<>();
+        while (rs.next()) {
+            //(int packId, String setName, int quantity)
+            cartItems.add( new CartItem(
+                    rs.getInt("packId"),
+                    rs.getString("setName"),
+                    rs.getInt("quantity")
+            ));
+        }
+        return cartItems;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
         return null;
     }
 
     // will update the quantity of an item in the cart
-    boolean updateCartItemQuantity (int cartId, int packId, int quantity) {
+    boolean updateCartItemQuantity (int userId, int packId, int quantity) {
+        try {
+            String sql = "UPDATE cart set quantity = ? where userId = ? AND packId = ?";
+            PreparedStatement psmt = connection.prepareStatement(sql);
+            psmt.setInt(1, quantity);
+            psmt.setInt(2, userId);
+            psmt.setInt(3, packId);
+            int numberUpdated = psmt.executeUpdate();
+            return numberUpdated > 0;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
         return false;
     }
 
-    // will remove a cart item from the cart with the cartId
-    boolean removeFromCart (int cartId) {
+    // will remove a cart item from the cart with the userId and packUd
+    boolean removeFromCart (int userId, int packId) {
+        try {
+            String sql = "DELETE FROM cart where userId = ? AND packId = ?";
+            PreparedStatement psmt = connection.prepareStatement(sql);
+            psmt.setInt(1, userId);
+            psmt.setInt(2, packId);
+            int numberUpdated = psmt.executeUpdate();
+            return numberUpdated > 0;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
         return false;
     }
 
     // will empty the entire cart of a user with userId
     boolean emptyUserCartByUserId (int userId) {
+        try {
+            String sql = "DELETE FROM cart where userId = ?";
+            PreparedStatement psmt = connection.prepareStatement(sql);
+            psmt.setInt(1, userId);
+            int numberUpdated = psmt.executeUpdate();
+            return numberUpdated > 0;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
         return false;
     }
 
     // adds an item to the cart, will return cartId
-    int addItemToCart (int userId, Pack pack, int quantity) {
-        return 0;
+    boolean addItemToCart (int userId, int packId, int quantity) {
+        try {
+            String sql = "INSERT INTO cart (userId, packId, quantity) VALUES (?,?,?)";
+            PreparedStatement psmt = connection.prepareStatement(sql);
+            psmt.setInt(1, userId);
+            psmt.setInt(2, packId);
+            psmt.setInt(3, quantity);
+            int numberUpdated = psmt.executeUpdate();
+            return numberUpdated > 0;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
     }
 }
