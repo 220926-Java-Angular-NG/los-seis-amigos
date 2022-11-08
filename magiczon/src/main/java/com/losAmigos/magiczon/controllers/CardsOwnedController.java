30e798f76a2dbcedab6bea95e2e0264931c9aec7
DrLokin <daniel.lanzav@gmail.com>
2022-11-08 10:48:33 -0500

    Fixed CardsOwned controller

--- magiczon/src/main/java/com/losAmigos/magiczon/controllers/CardsOwnedController.java
+++ magiczon/src/main/java/com/losAmigos/magiczon/controllers/CardsOwnedController.java
@@ -3,14 +3,13 @@ package com.losAmigos.magiczon.controllers;
 import com.losAmigos.magiczon.models.CardsOwned;
 import com.losAmigos.magiczon.services.CardsOwnedService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.websocket.server.PathParam;
 import java.util.List;
 
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/collection")
 public class CardsOwnedController {
